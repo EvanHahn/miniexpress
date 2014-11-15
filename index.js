2a29d9218658803d8b56af9c0fe30e216f9ae047
Evan Hahn <me@evanhahn.com>
2014-11-15 16:51:31 -0500

    routes

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 require('object.assign').shim();
+var http = require('http');
+var util = require('util');
 
 var prototype = {
   handle: function handle(req, res) {
@@ -11,11 +13,36 @@ var prototype = {
   },
   use: function use(handler) {
     this.stack.push(handler);
+  },
+  listen: function listen() {
+    var server = http.createServer(this);
+    server.listen.apply(server, arguments);
   }
 };
 
+['get', 'head', 'options', 'post', 'put', 'delete'].forEach(function(method) {
+  prototype[method] = function(route, handler) {
+    this.stack.push(function(req, res, next) {
+      var methodMatches = method === req.method.toLowerCase();
+      var routeMatches;
+      if (util.isRegExp(route)) {
+        routeMatches = route.test(req.url);
+      } else {
+        routeMatches = route == req.url;
+      }
+      if (methodMatches || routeMatches) {
+        handler(req, res, next);
+      } else {
+        next();
+      }
+    });
+  };
+});
+
 module.exports = function express() {
   function app(req, res) { app.handle(req, res); }
-  Object.assign(app, prototype, { stack: [] });
+  Object.assign(app, prototype, {
+    stack: []
+  });
   return app;
 };
